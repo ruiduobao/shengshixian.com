434ec6c2130704891c975f12fa09b93bf184037b
xugaoyi <894072666@qq.com>
2020-05-31 11:04:36 +0800

    更新：插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -94,13 +94,13 @@ module.exports = {
     ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
       thirdparty: [ // 可选，默认 []
         {
-          title: '在MDN中搜索',
-          frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
+          title: '在GitHub中搜索',
+          frontUrl: 'https://github.com/search?q=', // 搜索链接的前面部分
           behindUrl: '' // 搜索链接的后面部分，可选，默认 ''
         },
         {
-          title: '在Vue API中搜索',
-          frontUrl: 'https://cn.vuejs.org/v2/api/#',
+          title: '在npm中搜索',
+          frontUrl: 'https://www.npmjs.com/search?q=',
         },
         {
           title: '在Bing中搜索',
