fb36ae1d4d3f14f17b516d36869681a8ae819e38
xugaoyi <894072666@qq.com>
2020-08-12 20:57:09 +0800

    add 版本升级

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,7 +17,7 @@ module.exports = {
     lineNumbers: true // 代码行号
   },
   theme: 'vdoing', // 使用依赖包主题
-  // theme: require.resolve('../../vuepress-theme-vdoing'), // 使用本地主题
+  // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
   themeConfig: { // 主题配置
     nav,
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
