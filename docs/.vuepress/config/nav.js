06899b036bfc0bd10450c66336b868704cfbb11b
xugaoyi <894072666@qq.com>
2020-09-21 22:26:04 +0800

    添加案例 && 优化文档

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,36 +1,42 @@
 module.exports = [
-  {text: '首页', link: '/'},
-  {text: '指南', link: '/pages/a2f161/', items: [
-    {text: '主题初衷与诞生', link: '/pages/52d5c3/'},
-    {text: '介绍', link: '/pages/a2f161/'},
-    {text: '快速上手', link: '/pages/793dcb/'},
-    {text: '目录结构', link: '/pages/2f674a/'},
-    {text: '构建结构化站点的核心配置和约定', link: '/pages/33d574/'},
-    {text: '自动生成front matter', link: '/pages/088c16/'},
-    {text: 'Markdown 容器', link: '/pages/d0d7eb/'},
+  { text: '首页', link: '/' },
+  {
+    text: '指南', link: '/pages/a2f161/', items: [
+      { text: '主题初衷与诞生', link: '/pages/52d5c3/' },
+      { text: '介绍', link: '/pages/a2f161/' },
+      { text: '快速上手', link: '/pages/793dcb/' },
+      { text: '目录结构', link: '/pages/2f674a/' },
+      { text: '核心配置和约定', link: '/pages/33d574/' },
+      { text: '自动生成front matter', link: '/pages/088c16/' },
+      { text: 'Markdown 容器', link: '/pages/d0d7eb/' },
 
-    {text: '相关文章', items: [
-      {text: '批量操作front matter工具', link: '/pages/2b8e22/'},
-      {text: '部署', link: '/pages/0fc1d2/'},
-      {text: '关于写文章和H1标题', link: '/pages/9ae0bd/'},
-      {text: '关于博客搭建与管理', link: '/pages/26997d/'},
-      {text: '在线编辑和新增文章的方法', link: '/pages/c5a54d/'},
-    ]}
-  ]},
-  {text: '配置', link: '/pages/a20ce8/', items: [
-    {text: '主题配置', link: '/pages/a20ce8/'},
-    {text: '首页配置', link: '/pages/f14bdb/'},
-    {text: 'front matter配置', link: '/pages/3216b0/'},
-    {text: '目录页配置', link: '/pages/54651a/'},
-    {text: '添加摘要', link: '/pages/1cc523/'},
-    {text: '修改主题颜色和样式', link: '/pages/f51918/'},
-    {text: '评论栏', link: '/pages/ce175c/'},
-  ]},
-  {text: '插件', link: '/pages/db78e2/'},
-  {text: '案例', link: '/pages/5d571c/'},
-  {text: '问答', link: '/pages/9cc27d/'},
-  {text: '💖支持', link: '/pages/1b12ed/'},
-  {text: '反馈', link: '/pages/5dfce5/'},
+      {
+        text: '相关文章', items: [
+          { text: '批量操作front matter工具', link: '/pages/2b8e22/' },
+          { text: '部署', link: '/pages/0fc1d2/' },
+          { text: '关于写文章和H1标题', link: '/pages/9ae0bd/' },
+          { text: '关于博客搭建与管理', link: '/pages/26997d/' },
+          { text: '在线编辑和新增文章的方法', link: '/pages/c5a54d/' },
+        ]
+      }
+    ]
+  },
+  {
+    text: '配置', link: '/pages/a20ce8/', items: [
+      { text: '主题配置', link: '/pages/a20ce8/' },
+      { text: '首页配置', link: '/pages/f14bdb/' },
+      { text: 'front matter配置', link: '/pages/3216b0/' },
+      { text: '目录页配置', link: '/pages/54651a/' },
+      { text: '添加摘要', link: '/pages/1cc523/' },
+      { text: '修改主题颜色和样式', link: '/pages/f51918/' },
+      { text: '评论栏', link: '/pages/ce175c/' },
+    ]
+  },
+  { text: '插件', link: '/pages/db78e2/' },
+  { text: '案例', link: '/pages/5d571c/' },
+  { text: '问答', link: '/pages/9cc27d/' },
+  { text: '💖支持', link: '/pages/1b12ed/' },
+  { text: '反馈', link: '/pages/5dfce5/' },
   // {
   //   text: '前端',
   //   link: '/web/',  //目录页，vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
@@ -49,7 +55,7 @@ module.exports = [
   //   ]
   // },
   // {
-  //   text: '页面', 
+  //   text: '页面',
   //   link: '/ui/',
   //   items: [
   //     {text: 'HTML', link: '/pages/8309a5b876fc95e3/'},
@@ -67,7 +73,7 @@ module.exports = [
   //   ]
   // },
   // {
-  //   text: '更多', 
+  //   text: '更多',
   //   link: '/more/',
   //   items: [
   //     {text: '学习', link: '/pages/a8692ab3bdcb4588/'},
