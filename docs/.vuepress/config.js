4c1506dabfb70f090a6071fbdf445e579d33c058
xugaoyi <894072666@qq.com>
2020-12-22 15:57:14 +0800

    去掉编辑按钮

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -36,9 +36,9 @@ module.exports = {
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
 
-    docsDir: 'docs', // 编辑的文件夹
-    editLinks: true, // 编辑链接
-    editLinkText: '编辑',
+    // docsDir: 'docs', // 编辑的文件夹
+    // editLinks: true, // 编辑链接
+    // editLinkText: '编辑',
 
     // 以下配置是Vdoing主题改动的和新增的配置
     sidebar: { mode: 'structuring', collapsable: false }, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
