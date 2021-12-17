75c2534be1722be40a717f0294f8058223016821
xugaoyi <894072666@qq.com>
2021-12-17 15:34:02 +0800

    test: ad

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -2,26 +2,28 @@ export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
   options, // 附加到根实例的一些选项
   router, // 当前应用的路由实例
-  siteData // 站点元数据
+  siteData, // 站点元数据
+  isServer // 当前应用配置是处于 服务端渲染 或 客户端
 }) => {
-
-  // 用于监控在路由变化时检查广告拦截器 (to主题使用者：你可以去掉本文件的所有代码)
-  router.beforeEach((to, from, next) => {
-    next();
-    //check if wwads' fire function was blocked after document is ready with 3s timeout (waiting the ad loading)
-    try {
-      docReady(function () {
-        setTimeout(function () {
-          if (window._AdBlockInit === undefined) {
-            ABDetected();
-          }
-        }, 3000);
-      });
-    } catch (err) {
-      console.log(err);
-    }
-
-  })
+  if (!isServer) {
+    // 用于监控在路由变化时检查广告拦截器 (to主题使用者：你可以去掉本文件的所有代码)
+    router.beforeEach((to, from, next) => {
+      console.log(to);
+      next();
+      //check if wwads' fire function was blocked after document is ready with 3s timeout (waiting the ad loading)
+      try {
+        docReady(function () {
+          setTimeout(function () {
+            if (window._AdBlockInit === undefined) {
+              ABDetected();
+            }
+          }, 3000);
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    })
+  }
 }
 
 
