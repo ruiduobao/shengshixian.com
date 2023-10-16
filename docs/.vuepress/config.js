b10736c32722bab8876f8cdcc9f7d0671eb5cf25
kitmyfaceplease <kitmyfaceplease2@gmail.com>
2023-10-16 17:24:25 +0800

    你的提交消息

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -31,7 +31,7 @@ module.exports = {
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/logo.png', // 导航栏logo
-    repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+    repo: 'ruiduobao/shengshixian.com', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
 
@@ -63,27 +63,37 @@ module.exports = {
 
     author: { // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, href: String}
       name: '锐多宝', // 必需
-      href: 'https://github.com/xugaoyi' // 可选的
+      href: 'https://github.com/ruiduobao/shengshixian.com' // 可选的
     },
     social: { // 社交图标，显示于博主信息栏和页脚栏
       // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
       icons: [
+        // {
+        //   iconClass: 'icon-youjian',
+        //   title: '发邮件',
+        //   link: 'mailto:caaschengrui@163.com',
+        // },
+        // {
+        //   iconClass: 'icon-github',
+        //   title: 'github',
+        //   link: 'https://github.com/ruiduobao/gisrsdata.com',
+        // },
         {
-          iconClass: 'icon-youjian',
-          title: '发邮件',
-          link: 'mailto:894072666@qq.com'
+          iconClass: 'icon-weixin',
+          title: '微信',
+          link: 'http://pics.landcover100.com/pics//image/20211128020929.png',
         },
         {
-          iconClass: 'icon-github',
-          title: 'GitHub',
-          link: 'https://github.com/xugaoyi'
+          iconClass: 'icon-zhihu',
+          title: '知乎',
+          link: 'https://www.zhihu.com/people/justyoufengmei',
         },
-        {
-          iconClass: 'icon-erji',
-          title: '听音乐',
-          link: 'https://music.163.com/#/playlist?id=755597173'
-        }
-      ]
+        // {
+        //   iconClass: 'icon-csdn',
+        //   title: 'CSDN',
+        //   link: 'https://ruiduobao.blog.csdn.net/',
+        // },
+      ],
     },
     footer: { // 页脚信息
       createYear: 2019, // 博客创建年份
